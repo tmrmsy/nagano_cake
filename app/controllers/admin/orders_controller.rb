8cf56549002b6247e0bec8a4fb2d5098176c3dea
EC2 Default User <ec2-user@ip-172-31-1-219.ec2.internal>
2022-04-07 13:55:15 +0000

    [Finish]注文、製作ステータス追加完了

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -10,9 +10,7 @@ class Admin::OrdersController < ApplicationController
 
   def update
     order = Order.find(params[:id])
-    #order_detail = OrderDetail.find_by(params[:order_id][order.id])
     order.update(order_params)
-    #order_detail.update(order_detail_params)
     redirect_to admin_order_path(order.id)
   end
 
@@ -21,8 +19,5 @@ class Admin::OrdersController < ApplicationController
   def order_params
     params.require(:order).permit(:order_status)
   end
-
-  def order_detail_params
-    params.require(:order_detail).permit(:making_status)
-  end
+  
 end
